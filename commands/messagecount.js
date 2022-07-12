27ae6a23963cec6e7f472ed9ce52936e75e8298d
bldhf <pascalbracamonte@gmail.com>
2022-07-12 10:42:01 -0400

    i'm pretty sure the problem with messagecount was ID vs Id

--- commands/messagecount.js
+++ commands/messagecount.js
@@ -7,19 +7,19 @@ module.exports = {
 	aliases: ['msgcount', 'msgs', 'mc'],
 	hidden: false,
 	async execute(client, message, args) {
-		let levelSchema = await Levels.findOne({ userID: message.author.id })
+		let levelSchema = await Levels.findOne({ userId: message.author.id })
 		if (!levelSchema)
 			levelSchema = await new Levels({
-				userID: message.author.id,
+				userId: message.author.id,
 				xp: 0,
 				shopDiscount: 0,
 				msgDiscount: 0,
 			})
 				.save()
 				.catch((err) => console.log(err))
-		let messageSchema = await Messages.findOne({ userID: message.author.id })
+		let messageSchema = await Messages.findOne({ userId: message.author.id })
 		if (!messageSchema) {
-			messageSchema = new Messages({ userID: message.author.id, messages: 0 })
+			messageSchema = new Messages({ userId: message.author.id, messages: 0 })
 			await messageSchema.save().catch((err) => console.log(err))
 		}
 		return message.reply(`You have ${messageSchema.messages} messages out of ${50 - levelSchema.msgDiscount} required to gain Dogie Coins. Your current discount is ${levelSchema.msgDiscount}`)
