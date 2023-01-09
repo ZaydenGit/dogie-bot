d8c698cef425da9e6c078eb785c5325687436edd
ZaydenGit <zaydenmiddleton12@gmail.com>
2023-01-08 21:36:22 -0800

    updated discord.js added openai shit

---
+++ commands/ask.js
@@ -0,0 +1,35 @@
+const { Configuration, OpenAIApi } = require('openai')
+const cfg = new Configuration({
+	apiKey: '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',
+})
+module.exports = {
+	name: 'ask',
+	description: 'ask dogie a question :P',
+	aliases: ['question'],
+	hidden: false,
+	async execute(client, message, args) {
+		console.log(args.join(' '))
+		// const msgRef = message.reply('thinking...')
+		try {
+			const openai = new OpenAIApi(cfg)
+			const completion = await openai.createCompletion({
+				model: 'text-davinci-003',
+				prompt: args.join(' '),
+				max_tokens: 2048,
+				n: 1,
+				stop: null,
+				temperature: 0.7,
+			})
+			console.log(`>${completion.data.choices[0].text.trim()}`)
+			try {
+				message.reply(completion.data.choices[0].text.trim(), { split: true })
+			} catch (error) {
+				console.log(errpr)
+				message.reply('dogie had a lil oopsie (probably output too long will fix at later date)')
+			}
+		} catch (error) {
+			console.log(error)
+			message.reply('openai is currently overloaded :((( try later :((((')
+		}
+	},
+}
