d8d7e6800c818dc79e60a841ac4aacc23321606b
ZaydenGit <zaydenmiddleton12@gmail.com>
2024-09-14 04:32:38 -0700

    fix ?

---
+++ commands/ask.js
@@ -0,0 +1,9 @@
+const dogieMonday = require('../events/messageCreate.js')
+// import { d } from '../events/messageCreate.js'
+module.exports = {
+	name: 'monday',
+	description: 'moday',
+	aliases: ['moday'],
+	hidden: false,
+	async execute(client, message, args) {},
+}
