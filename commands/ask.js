d16949a297732d77b05ef71c75d242144cc11eef
ZaydenGit <zaydenmiddleton12@gmail.com>
2024-09-14 15:03:26 -0700

    fixed little thing

--- commands/ask.js
+++ commands/ask.js
@@ -1,9 +1,11 @@
 const dogieMonday = require('../events/messageCreate.js')
 // import { d } from '../events/messageCreate.js'
 module.exports = {
-	name: 'monday',
-	description: 'moday',
-	aliases: ['moday'],
+	name: 'ask',
+	description: 'gibbity',
+	aliases: ['chad gibbity'],
 	hidden: false,
-	async execute(client, message, args) {},
+	async execute(client, message, args) {
+		message.reply('not ready yet :p')
+	},
 }
