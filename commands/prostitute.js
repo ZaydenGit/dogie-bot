5c6c28cfdb89731e36dc44bf5dc3bdd00e503669
ZaydenGit <zaydenmiddleton12@gmail.com>
2025-05-24 20:37:59 -0700

    akin to 9/11

--- commands/prostitute.js
+++ commands/prostitute.js
@@ -5,6 +5,16 @@ module.exports = {
 	aliases: ['getoutofdebtfree', 'debt', 'prostitution'],
 	hidden: false,
 	async execute(client, message, args) {
+		//cd
+		const cooldownTime = 60 * 1000 //15s
+		const now = Date.now()
+		const cmdLastUsed = cooldowns.get(message.author.id)
+		if (cmdLastUsed && now - cmdLastUsed < cooldownTime) {
+			const timeLeft = Math.ceil((cooldownTime - (now - cmdLastUsed)) / 1000)
+			if (between(0, 1) > 0.98) return message.reply(`Due to complaints and an ongoing investigation from a certain federal bureau you must wait ${timeLeft} seconds before gambling again.`)
+			else return message.reply(`You must wait ${timeLeft} second(s) before using this again.`)
+		}
+		cooldowns.set(message.author.id, now)
 		function between(min, max) {
 			return Math.floor(Math.random() * (max - min + 1) + min)
 		}
@@ -18,15 +28,22 @@ module.exports = {
 				serverId: message.guild.id,
 				money: 0,
 			})
-			await money.save().catch((e) => console.log(e))
+			await moneySchema.save().catch((e) => console.log(e))
 		}
 		if (Math.sign(moneySchema.money) != -1) return message.channel.send('You must be in debt (negative money) to use this command.')
 		let tip = Math.round(between(1, 4))
 		moneySchema.money = (tip - 1) * 1000
 		moneySchema.save().catch((e) => console.log(e))
-		if (tip === 1) return message.channel.send(`You were pretty bad. Dogie doesn't leave a tip, but still gets you out of debt.`)
-		if (tip === 2) return message.channel.send(`You were alright. Dogie tips you ${(tip - 1) * 1000} Coins \<:dogie:746903359071584337>`)
-		if (tip === 3) return message.channel.send(`You were great!. Dogie tips you ${(tip - 1) * 1000} Coins \<:happydogie:733840389840306176>`)
-		if (tip === 4) return message.channel.send(`You were amazing!. Dogie tips you ${(tip - 1) * 1000} Coins \<:angeldogie:777888818019172382>`)
+		switch (tip) {
+			case 1:
+				return message.channel.send(`You were pretty bad. Dogie doesn't leave a tip, but still gets you out of debt.`)
+			case 2:
+				return message.channel.send(`You were alright. Dogie tips you ${(tip - 1) * 1000} Coins \<:dogie:746903359071584337>`)
+			case 3:
+				return message.channel.send(`You were great!. Dogie tips you ${(tip - 1) * 1000} Coins \<:happydogie:733840389840306176>`)
+			case 4:
+				return message.channel.send(`You were amazing!. Dogie tips you ${(tip - 1) * 1000} Coins \<:angeldogie:777888818019172382>`)
+		}
+		setTimeout(() => cooldowns.delete(message.author.id), cooldownTime)
 	},
 }
