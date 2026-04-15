582b3c481e1364b3bdd8d4b4d1c62e773ef5f3e9
Zayden <78576649+ZaydenGit@users.noreply.github.com>
2026-04-15 13:16:58 -0700

    fixed declaration of ranVal with a let

--- src/commands/funny.js
+++ src/commands/funny.js
@@ -5,7 +5,7 @@ export default {
 	aliases: ["funny"],
 	hidden: false,
 	async execute(client, message, args) {
-		ranVal = between(0, 1);
+		let ranVal = between(0, 1);
 		if (ranVal <= 0.22) message.reply("lol");
 		if (ranVal > 0.22 && ranVal <= 0.66) message.reply("rofl");
 		if (ranVal > 0.66 && ranVal <= 0.98) message.reply("trololol");
